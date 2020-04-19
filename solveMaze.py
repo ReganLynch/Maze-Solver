e4e7f4a9f4a5a669b455c6d9ce8da2c0d0f7d1e6
ReganLynch <reganlynch3@gmail.com>
2020-04-18 21:59:42 -0300

    started maze solver

---
+++ solveMaze.py
@@ -0,0 +1,21 @@
+import sys
+from os import path
+
+def main():
+    #check that the file path was passed
+    if len(sys.argv) != 2:
+        print('invalid number of arguments passed')
+        print('usage: solveMaze.py <file_path>')
+        exit()
+    file_path = str(sys.argv[1])
+    #check if the passed file is valid
+    if not path.exists(file_path):
+        print('invalid file path')
+        exit()
+    #check that the input file is a .bmp
+    file_name, file_extension = path.splitext(file_path)
+    print(file_extension)
+
+
+if __name__ == '__main__':
+    main()
